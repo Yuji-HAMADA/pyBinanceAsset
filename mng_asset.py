4a23b295daf66ea0a203c4bce9db0abfee89dcad
Yuji Hamada <yuji.tky.ykhm@gmail.com>
2022-09-18 13:12:46 +0900

    modified:   mng_asset.py

--- mng_asset.py
+++ mng_asset.py
@@ -2,27 +2,21 @@ from binance.spot import Spot
 import json
 import settings
 
-# api key/secret are required for user data endpoints
+
+"""
+api key/secret are required for user data endpoints
+They are expected to be set in setting.py of the same folder like below
+API_KEY='apikeyapikey'
+SECRET_KEY='secretkeysecretkey'
+"""
 client = Spot(key=settings.API_KEY, secret=settings.SECRET_KEY)
 
-# Get account and balance information
-account = client.account()
-#print(account)
-
-# Define spot_n_savings dict
-spot_n_savings = {}
-
-# Set spot_n_savings dict for non-empty crypt
-for balance in account['balances']:
-    amount = float((balance['free']))
-    if amount > 0:
-        spot_n_savings[balance['asset']] = amount
-
-print(spot_n_savings)
-path1 = './test1.json'
-json_file1 = open(path1, mode="w")
-json.dump(spot_n_savings, json_file1)
-json_file1.close()
+spot = client.account_snapshot('SPOT')
+path4 = './spot.json'
+json_file4 = open(path4, mode="w")
+json.dump(spot, json_file4)
+json_file4.close()
+print(spot)
 
 staking = client.staking_product_position('STAKING')
 #print(staking)
@@ -32,14 +26,8 @@ json.dump(staking, json_file2)
 json_file2.close()
 
 saving = client.savings_flexible_product_position()
-print(saving)
+#print(saving)
 path3 = './test_saving.json'
 json_file3 = open(path3, mode="w")
 json.dump(saving, json_file3)
 json_file3.close()
-
-#snaptshot = client.account_snapshot('SPOT')
-#print(snaptshot)
-
-#account_status = client.account_status()
-#print(account_status)
