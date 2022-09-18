32501d2c716f3956c726fcd8bf1792e30aa58eee
Yuji Hamada <yuji.tky.ykhm@gmail.com>
2022-09-19 08:24:50 +0900

    Unnecessary comments deleted:   mng_asset.py

--- mng_asset.py
+++ mng_asset.py
@@ -11,22 +11,16 @@ SECRET_KEY='secretkeysecretkey'
 client = Spot(key=settings.API_KEY, secret=settings.SECRET_KEY)
 
 spot = client.account_snapshot('SPOT')
-#path4 = './spot.json'
 spotFile = open('./spot.json', mode="w")
 json.dump(spot, spotFile)
 spotFile.close()
-#print(spot)
 
 staking = client.staking_product_position('STAKING')
-#print(staking)
-#path2 = './staking.json'
 fileStaking = open('./staking.json', mode="w")
 json.dump(staking, fileStaking)
 fileStaking.close()
 
 saving = client.savings_flexible_product_position()
-#print(saving)
-#path3 = './test_saving.json'
 fileFlexible = open('./saving.json', mode="w")
 json.dump(saving, fileFlexible)
 fileFlexible.close()
